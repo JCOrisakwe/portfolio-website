6423d86e3b2b5c015ffd6fff8168b93a1420e6e6
JCOrisakwe <oriz4work@gmail.com>
2026-03-13 03:27:51 +0100

    add custom sidebar;

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+const navToggler = document.querySelector(".navbar-toggler");
+const backdropOverlay = document.querySelector(".overlay");
+const navDrawer = document.querySelector("#main-nav__links");
+
+const isDrawerVisible = () => navDrawer.classList.contains("drawer-open");
+const setDrawerVisibility = function (action = "add") {
+  navDrawer.classList[action]("drawer-open");
+  backdropOverlay.classList[action]("overlay-visible");
+  navDrawer.parentElement.classList[action]("no-blur");
+};
+
+document.addEventListener("click", function (e) {
+  if (!navDrawer.contains(e.target) && !navToggler.contains(e.target)) {
+    setDrawerVisibility("remove");
+  }
+});
+
+navToggler.addEventListener("click", function () {
+  setDrawerVisibility(isDrawerVisible() ? "remove" : "add");
+});
+
+document.querySelectorAll("#main-nav__links .nav-item").forEach((li, id) => {
+  const numSpan = document.createElement("span");
+  numSpan.innerText = `0${id + 1}.`;
+  numSpan.classList.add("fs-6", "text");
+  li.prepend(numSpan);
+
+  li.querySelector("a").addEventListener("click", () =>
+    setDrawerVisibility("remove"),
+  );
+});
+
+window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  if (e.matches) setDrawerVisibility("remove");
+});
