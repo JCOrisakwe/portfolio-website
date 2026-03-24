b0f3c9c4f842177f526d77db22d2e09260eead0d
Joseph Orisakwe <oriz4work@gmail.com>
2026-03-25 00:17:27 +0100

    Fix nav drawer bugs; add close button to nav drawer

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,42 @@
 const navToggler = document.querySelector(".navbar-toggler");
+const icon = navToggler.querySelector("i");
 const backdropOverlay = document.querySelector(".overlay");
 const navDrawer = document.querySelector("#main-nav__links");
 
+if (!navToggler || !backdropOverlay || !navDrawer)
+  throw new Error("Nav elements missing");
+
 const isDrawerVisible = () => navDrawer.classList.contains("drawer-open");
-const setDrawerVisibility = function (action = "add") {
-  navDrawer.classList[action]("drawer-open");
-  navDrawer.classList[action]("border-start");
-  backdropOverlay.classList[action]("vh-100");
-  document.body.classList[action]("overflow-hidden");
+const toggleDrawer = (isOpen) => {
+  navDrawer.classList.toggle("drawer-open", isOpen);
+  navDrawer.classList.toggle("border-start", isOpen);
+  backdropOverlay.classList.toggle("vh-100", isOpen);
+  icon.classList.toggle("fa-bars", !isOpen);
+  icon.classList.toggle("fa-x", isOpen);
 };
 
 document.addEventListener("click", function (e) {
   if (!navDrawer.contains(e.target) && !navToggler.contains(e.target)) {
-    setDrawerVisibility("remove");
+    toggleDrawer(false);
   }
 });
 
-navToggler.addEventListener("click", function () {
-  setDrawerVisibility(isDrawerVisible() ? "remove" : "add");
+navToggler.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDrawer(!isDrawerVisible());
 });
 
-document.querySelectorAll("#main-nav__links .nav-item").forEach((li, id) => {
+document.querySelectorAll("#main-nav__links .nav-item").forEach((li, index) => {
   const numSpan = document.createElement("span");
-  numSpan.innerText = `0${id + 1}.`;
+  numSpan.innerText = `${String(index + 1).padStart(2, "0")}.`;
   numSpan.classList.add("fs-6", "text");
   li.prepend(numSpan);
-
-  li.querySelector("a").addEventListener("click", () =>
-    setDrawerVisibility("remove"),
-  );
+  li.querySelector("a").addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleDrawer(false);
+  });
 });
 
 window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
-  if (e.matches) setDrawerVisibility("remove");
+  if (e.matches) toggleDrawer(false);
 });
