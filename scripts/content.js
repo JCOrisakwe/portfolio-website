c0292ccd801dfb6ae3612ccf6201206e3ee0d739
JCOrisakwe <oriz4work@gmail.com>
2026-08-25 07:52:55 +0100

    feat: add technologies and projects data structure to content module

---
+++ scripts/content.js
@@ -0,0 +1,43 @@
+export const TECHNOLOGIES = [
+  { name: "HTML5" },
+  { name: "CSS3" },
+  { name: "Bootstrap 5" },
+  { name: "Material UI" },
+  { name: "Responsive Design" },
+  { name: "JavaScript" },
+  { name: "React" },
+  { name: "Vite" },
+  { name: "Axios / Fetch" },
+  { name: "Node.js" },
+  { name: "Express.js" },
+  { name: "RESTful APIs" },
+  { name: "EJS Templating" },
+  { name: "Python" },
+  { name: "Django" },
+  { name: "MongoDB" },
+  { name: "Mongoose ODM" },
+  { name: "Passport.js (Auth)" },
+  { name: "JOI Validation" },
+  { name: "Helmet.js (Security)" },
+  { name: "Cloudinary (Image Upload)" },
+  { name: "MapTiler (Maps)" },
+  { name: "Git & Terminal" },
+  { name: "Render / Heroku" },
+];
+
+export const PROJECTS = [
+  {
+    name: "goated",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odio veniam recusandae assumenda sunt libero ad ut officia doloremque aliquam, aspernatur totam ea numquam quod adipisci animi delectus eius voluptates.`,
+  },
+  {
+    name: "malesuada",
+    description:
+      "Pellentesque eget orci ultricies, feugiat elit a, efficitur tellus. Proin vitae felis ac ex aliquam rhoncus. Phasellus feugiat augue purus, in hendrerit felis finibus et. Nunc semper nisi consectetur tincidunt posuere. Nullam sollicitudin metus non.",
+  },
+  {
+    name: "fringilla",
+    description:
+      "Lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis.",
+  },
+];
